40d785bf6393976a30d83b2dd9990771e2b03595
yohan_01 <84216814+yunj00@users.noreply.github.com>
2021-05-29 13:49:19 +0900

    Add files via upload

---
+++ DAY_07/outfunction01.js
@@ -0,0 +1,7 @@
+function f1() {
+    alert('함수1을 호출함')
+}
+
+function f2() {
+    alert('함수2을 호출함')
+}
---
+++ DAY_07/outfunction02.js
@@ -0,0 +1,7 @@
+function f3() {
+    alert('함수3을 호출하였습니다')
+}
+
+function f4() {
+    alert('함수4을 호출하였습니다')
+}
